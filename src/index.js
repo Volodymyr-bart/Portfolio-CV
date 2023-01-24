e8f05b3896eb7d4eba8b4ab96dbdb91ff6c69417
Volodymyr Bortokhov <bartzh1991@gmail.com>
2023-01-24 13:41:29 +0200

    add librares

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="Portfolio">
+    <BrowserRouter basename="Portfolio-CV">
       <App />
     </BrowserRouter>
   </React.StrictMode>
