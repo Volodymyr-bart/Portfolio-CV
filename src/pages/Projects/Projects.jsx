c4dd7922fca0af4c132c66ca8b33934e3aa69dd2
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-06 19:12:57 +0200

    view

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import Container from 'components/Container/Container';
-import Pagination from 'components/Pagination/Pagination';
+// import Pagination from 'components/Pagination/Pagination';
 import Section from 'components/Section/Section';
 import Title from 'components/Title/Title';
 // import { Project } from 'components/Project/Project';
 // import { ProjectsList } from './Projects.styled';
-import { projects } from 'helpers/projectsList';
+// import { projects } from 'helpers/projectsList';
 
 export const Projects = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+  // const [currentPage, setCurrentPage] = useState(1);
   // const [itemsPerPage, setItemsPerPage] = useState(9);
   // const itemsPerPage = 9;
   // const lastItemIndex = currentPage * itemsPerPage;
   // const firstItemIndex = lastItemIndex - itemsPerPage;
   // const currentItem = projects.slice(firstItemIndex, lastItemIndex);
 
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+  // const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <>
