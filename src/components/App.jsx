0a6329bfd550a7b9e5fabea860daf290665e44e9
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-06 19:08:26 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import './../styles/main.css';
 import './../styles/main.css';
-import ScrollToTop from 'utils/scrollToTop';
+// import ScrollToTop from 'utils/scrollToTop';
 import Navbar from './Navbar/Navbar';
 import Home from 'pages/Home/Home';
-import Projects from 'pages/Projects/Projects';
+import { Projects } from 'pages/Projects/Projects';
 import ProjectPage from 'pages/ProjectPage/ProjectPage';
 import Education from 'pages/Education/Education';
 import Contacts from 'pages/Contacts/Contacts';
@@ -13,7 +13,7 @@ import Footer from './Footer/Footer';
 export const App = () => {
   return (
     <div className="App">
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react';
+
 import Container from 'components/Container/Container';
 import Pagination from 'components/Pagination/Pagination';
 import Section from 'components/Section/Section';
 import Title from 'components/Title/Title';
-
-import { projects } from './../../helpers/projectsList';
-import { ProjectsList } from './Projects.styled';
 import { Project } from 'components/Project/Project';
+import { ProjectsList } from './Projects.styled';
+import { projects } from 'helpers/projectsList';
 
-const Projects = () => {
+export const Projects = () => {
   const [currentPage, setCurrentPage] = useState(1);
   // const [itemsPerPage, setItemsPerPage] = useState(9);
   const itemsPerPage = 9;
-
   const lastItemIndex = currentPage * itemsPerPage;
-
   const firstItemIndex = lastItemIndex - itemsPerPage;
-
   const currentItem = projects.slice(firstItemIndex, lastItemIndex);
 
   const paginate = pageNumber => setCurrentPage(pageNumber);
@@ -50,5 +47,3 @@ const Projects = () => {
     </>
   );
 };
-
-export default Projects;
